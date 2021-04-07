c05cf035cb5a4cba352c90e4c71fcce7086d4769
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-04-07 20:59:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// for(var i = 1; i<=100; i=i+10){
+//     // document.write(i,"<br>")
+//     for(var a=i; a< i+10; a++){
+//         document.write(a," ")
+//     }
+//     document.write("<br>")
+// }
+// var names = "saad"
+// var userInput = prompt("Enter Name")
+// var newOne = userInput.toLowerCase();
+// if(newOne ==  names){
+//     alert("Han bhai sahi he")
+// }
+// else{
+//     alert("nahi jani case sensitive ka masla he")
+// }
+
+
+// var names = "Saad"
+// console.log(names.indexOf("d"))
+
+// console.log(names[0])
+// console.log(names[1]) 
+// console.log(names[2]) 
+// console.log(names[3])
+// var newOne = names.slice(1,3)
+// console.log(newOne) 
+
+
+// console.log(names.length);
+
+
+
+// var array = ["Saad", "Ali","acha","Suno"]
+// console.log(array.lastIndexOf("Suno"))
+// // var bn = "Saad Ali"
+// // console.log(bn.indexOf("i"))
+// // // console.log(nb)
+
+// var array = ["Saad",""]
+// var names = "Saad"
+// console.log(names.indexOf("a"))
+// var banadia = names.slice(2,3)
+// console.log(banadia)
+
+
+
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+// console.log(names.length)
